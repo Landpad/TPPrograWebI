3e9592b832b1246b005aa03dbec4539260984e9e
landpad <20844115+Landpad@users.noreply.github.com>
2024-11-20 01:12:54 -0300

    Added popUp functionality to about Us button

--- script.js
+++ script.js
@@ -2,3 +2,33 @@ document.querySelector('.svg-container').addEventListener('click', function() {
     this.classList.toggle('opened');
     document.querySelector('.aContainer').classList.toggle('active');
 });
+
+let popUpBox = document.getElementById("popUp");
+let sobreNosotros = document.getElementById("sobreNosotros");
+let popUpContactanos = document.getElementById("popUpContactanos");
+let closeButton = document.getElementById("closeButton");
+
+closeButton.addEventListener("click", (e)=>
+    {
+        closePopUp();
+    });
+
+popUpContactanos.addEventListener("click", (e)=>
+    {
+        closePopUp();
+    });
+
+sobreNosotros.addEventListener("click", (e)=>
+    {
+        displayPopUp();
+    });
+
+function displayPopUp()
+{
+    popUpBox.classList.add("mostrarPopUp")
+};
+
+function closePopUp()
+{
+    popUpBox.classList.remove("mostrarPopUp");
+};
